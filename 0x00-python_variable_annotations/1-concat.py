a41f14b735d9b8f5ca934a43e2908cdbf9c333eb
Collins Kimani <mwas9446.ck@gmail.com>
2024-08-01 15:27:07 +0300

    implement concat function
    the function concatenates two strings passed into the function

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""module: 1-concat
+"""
+# Write a type-annotated function concat that takes a string
+# str1 and a string str2 as arguments and returns a concatenated string
+
+
+def concat(str1: str, str2: str) -> str:
+    """concatenates two strings"""
+    return str1 + str2
