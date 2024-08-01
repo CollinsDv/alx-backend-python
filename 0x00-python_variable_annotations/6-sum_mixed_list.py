da4a37d47b8dff22fca21975baeab98f36469690
Collins Kimani <mwas9446.ck@gmail.com>
2024-08-01 16:45:44 +0300

    Update sum_mixed_list function parameter type to Union[int, float]

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -6,6 +6,6 @@ from typing import List, Union
 # integers and floats and returns their sum as a float.
 
 
-def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """sums a list and returns result as a float"""
     return float(sum(mxd_lst))
