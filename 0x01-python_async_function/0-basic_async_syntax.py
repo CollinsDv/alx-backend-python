0747f2fc02c0ceacde486ecc4dec8a0ce4577ce3
Collins Kimani <mwas9446.ck@gmail.com>
2024-08-05 17:11:08 +0300

    Update basic async syntax module with wait_random coroutine

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""module 0-basic_async_function
+"""
+import random
+import asyncio
+# Write an asynchronous coroutine that takes in an integer argument,
+#   - (max_delay,default value of 10)
+# named wait_random that waits for a random delay between 0 and max_delay
+#   - (included and float value)
+# seconds and eventually returns it.
+
+# Use the random module.
+
+
+async def wait_random(max_delay: int = 10):
+    """waits on random delay
+    Args:
+        max_delay (int) -> upper bound for delay operation
+    """
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
---
+++ 0x01-python_async_function/README.md
@@ -0,0 +1,5 @@
+## `Python - Async`
+involves delving into asynchronous python module that entails events that are made awaitable
+to improve general peformance.   
+All this made possible using `async` to generate coroutines and
+`await` keyword on the returned coroutine object
