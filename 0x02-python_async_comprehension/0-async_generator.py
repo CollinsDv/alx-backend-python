3b3b123235ec178236b7a5eaca3b1910b95318ec
Collins Kimani <mwas9446.ck@gmail.com>
2024-08-06 17:14:09 +0300

    add file 1 and handle coding style in file 0
    file 1 handles async comprehension from an async function

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,12 +3,13 @@
 """
 
 # Write a coroutine called async_generator that takes no arguments.
-# 
+#
 # The coroutine will loop 10 times, each time asynchronously wait 1 second,
 # then yield a random number between 0 and 10. Use the random module.
 
 import random
 import asyncio
+
 
 async def async_generator():
     """coroutine loops and yields a number"""
