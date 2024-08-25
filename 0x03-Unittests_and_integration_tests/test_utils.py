aca6f946a4a47590997a86c72e8318ae0fbb836a
Collins Kimani <mwas9446.ck@gmail.com>
2024-08-26 00:31:27 +0300

    Add unit tests for assertion of keyerror

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,22 +1,11 @@
 #!/usr/bin/env python3
 """
 Familiarize yourself with the utils.access_nested_map
-Create a TestAccessNestedMap class that inherits from
-unittest.TestCase.
 
-Implement the TestAccessNestedMap.test_access_nested_map
-method to test that the method returns what it is supposed to.
-
-Decorate the method with @parameterized.expand to test the function
-for following inputs:
-
+Test inputs:
     nested_map={"a": 1}, path=("a",)
     nested_map={"a": {"b": 2}}, path=("a",)
     nested_map={"a": {"b": 2}}, path=("a", "b")
-For each of these inputs, test with assertEqual that the function
-returns the expected result.
-
-The body of the test method should not be longer than 2 lines.
 """
 import unittest
 from parameterized import parameterized 
@@ -33,3 +22,12 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, _map, path, result):
         """takes parameterized args"""
         self.assertEqual(access_nested_map(_map, path), result)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, _map, path):
+        """test exception raises"""
+        with self.assertRaises(KeyError):
+            access_nested_map(_map, path)
