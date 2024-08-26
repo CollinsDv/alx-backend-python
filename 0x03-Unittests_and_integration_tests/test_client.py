b12fe07efa6cb0b673d9e1785d577a079661892e
Collins Kimani <mwas9446.ck@gmail.com>
2024-08-26 14:18:59 +0300

    refactor code to meet PEP guidelines

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -28,15 +28,20 @@ class TestGithubOrgClient(unittest.TestCase):
     """
     memoize turns methods into properties(mock.PropertyMock).
 
-    Implement the test_public_repos_url method to unit-test GithubOrgClient._public_repos_url.
+    Implement the test_public_repos_url method to unit-test
+        GithubOrgClient._public_repos_url.
 
-    Use patch as a context manager to patch GithubOrgClient.org and make it return a known payload.
+    Use patch as a context manager to patch GithubOrgClient.org and
+        make it return a known payload.
 
-    Test that the result of _public_repos_url is the expected one based on the mocked payload.
+    Test that the result of _public_repos_url is the expected one
+        based on the mocked payload.
     """
     def test_public_repos_url(self):
         """tests GithubOrgClient._public_repos_url"""
-        with unittest.mock.patch("client.GithubOrgClient.org", new_callable=unittest.mock.PropertyMock) as mock_org:
+        with unittest.mock.patch("client.GithubOrgClient.org",
+                                 new_callable=unittest.mock.PropertyMock
+                                 ) as mock_org:
             mock_org.return_value = {"repos_url": True}
 
-            assert GithubOrgClient('Google')._public_repos_url == True
+            assert GithubOrgClient('Google')._public_repos_url
