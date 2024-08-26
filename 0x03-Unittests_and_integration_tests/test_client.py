3daa34420181ab296e918ad3b51a2cd3f6b1eae7
Collins Kimani <mwas9446.ck@gmail.com>
2024-08-26 22:47:04 +0300

    test GithubOrgClient.public_repos integration with fixtures

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -6,6 +6,7 @@ import unittest
 import unittest.mock
 from client import GithubOrgClient
 from parameterized import parameterized
+from requests import HTTPError
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -97,3 +98,67 @@ class TestGithubOrgClient(unittest.TestCase):
         """tests GithubOrgClient.has_license"""
         assert GithubOrgClient('any').has_license(
             repos, license) == expected
+
+
+"""
+We want to test the GithubOrgClient.public_repos method in an
+    integration test. That means that we will only mock code that
+    sends external requests.
+
+Create the TestIntegrationGithubOrgClient(unittest.TestCase)
+    class and implement the setUpClass and tearDownClass which
+    are part of the unittest.TestCase API.
+
+Use @parameterized_class to decorate the class and parameterize
+    it with fixtures found in fixtures.py. The file contains the
+    following fixtures:
+    org_payload, repos_payload, expected_repos, apache2_repos
+
+The setupClass should mock requests.get to return example payloads
+    found in the fixtures.
+
+Use patch to start a patcher named get_patcher, and use side_effect
+    to make sure the mock of requests.get(url).json() returns the
+    correct fixtures for the various values of url that you anticipate
+    to receive.
+
+Implement the tearDownClass class method to stop the patcher.
+"""
+class TestIntegrationGithubOrgClient(unittest.TestCase):
+    """Performs integration tests for the `GithubOrgClient` class."""
+    @classmethod
+    def setUpClass(cls) -> None:
+        """Sets up class fixtures before running tests."""
+        route_payload = {
+            'https://api.github.com/orgs/google': cls.org_payload,
+            'https://api.github.com/orgs/google/repos': cls.repos_payload,
+        }
+
+        def get_payload(url):
+            if url in route_payload:
+                return unittest.mock.Mock(
+                    **{'json.return_value': route_payload[url]})
+            return HTTPError
+
+        cls.get_patcher = unittest.mock.patch(
+            "requests.get", side_effect=get_payload)
+        cls.get_patcher.start()
+
+    def test_public_repos(self) -> None:
+        """Tests the `public_repos` method."""
+        self.assertEqual(
+            GithubOrgClient("google").public_repos(),
+            self.expected_repos,
+        )
+
+    def test_public_repos_with_license(self) -> None:
+        """Tests the `public_repos` method with a license."""
+        self.assertEqual(
+            GithubOrgClient("google").public_repos(license="apache-2.0"),
+            self.apache2_repos,
+        )
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        """Removes the class fixtures after running all tests."""
+        cls.get_patcher.stop()
